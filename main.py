3afeec23906ca5f546d984e2b27c9c94458286ca
Robin Moser <mail@robinmoser.de>
2018-06-07 14:59:00 +0200

    Beschleunigung und Bewegung hinzufügen

--- main.py
+++ main.py
@@ -9,59 +9,78 @@ import traceback
 import spaceship
 
 # Init screen
-screen_size = [640,480]
+screen_size = [640, 480]
 
 pygame.display.init()
 pygame.display.set_caption("Asteroids")
-icon = pygame.Surface((1,1)); icon.set_alpha(0); pygame.display.set_icon(icon)
+icon = pygame.Surface((1, 1))
+icon.set_alpha(0)
+pygame.display.set_icon(icon)
 surface = pygame.display.set_mode(screen_size)
 
+
 def start_game():
-	
-	global spaceship
-	spaceship = spaceship.Spaceship([screen_size[0]/2.0,screen_size[1]/2.0])
+
+	global ship
+	ship = spaceship.Spaceship([screen_size[0]/2.0, screen_size[1]/2.0])
+
 
 def get_input(dt):
-	
+
 	keys_pressed = pygame.key.get_pressed()
 	for event in pygame.event.get():
-		if   event.type == QUIT: return False
+		if event.type == QUIT:
+			return False
 		elif event.type == KEYDOWN:
-			if   event.key == K_ESCAPE: return False
+			if event.key == K_ESCAPE:
+				return False
 
 	if keys_pressed[K_LEFT]:
-		spaceship.angle += 3.0
-	
-	if keys_pressed[K_RIGHT]:
-		spaceship.angle -= 3.0
+		ship.angle += 3.0
 
+	if keys_pressed[K_RIGHT]:
+		ship.angle -= 3.0
+
+	if keys_pressed[K_UP]:
+		ship.velocity[0] += dt * spaceship.Spaceship.speed * sin(radians(ship.angle))
+		ship.velocity[1] += dt * spaceship.Spaceship.speed * cos(radians(ship.angle))
+
+	if keys_pressed[K_DOWN]:
+		ship.velocity[0] *= 0.99
+		ship.velocity[1] *= 0.99
 	return True
+
 
 def update(dt):
 
-	spaceship.update(dt, screen_size)
+	ship.update(dt, screen_size)
 	return True
 
+
 def draw():
-	surface.fill((0,0,0))
-	spaceship.draw(surface)
+	surface.fill((0, 0, 0))
+	ship.draw(surface)
 	pygame.display.flip()
+
 
 def main():
 	global clock
-	
-	target_fps = 60
+
+	target_fps = 20
 	dt = 1.0/float(target_fps)
-	
+
 	start_game()
 	clock = pygame.time.Clock()
 
 	while True:
-		if not get_input(dt): break
-		if not update(dt): break
+		if not get_input(dt):
+			break
+		if not update(dt):
+			break
 		draw()
 		clock.tick(target_fps)
 	pygame.quit()
+
 
 if __name__ == "__main__":
 	try:
--- spaceship.py
+++ spaceship.py
@@ -1,23 +1,44 @@
 import pygame
 from math import *
 
+
 class Spaceship:
-	
+
+	speed = 300.0
+
 	def __init__(self, position):
 
 		self.position = list(position)
-		self.velocity = [1.0,0.0]
+		self.velocity = [1.0, 0.0]
 		self.angle = 180.0
+		self.real_points = []
 
 		self.rel_points = [[0, 20], [-140, 20], [180, 7.5], [140, 20]]
 		scale = 0.6
 		for i in range(len(self.rel_points)):
-			self.rel_points[i] = (radians(self.rel_points[i][0]),scale*self.rel_points[i][1])
+			self.rel_points[i] = (radians(self.rel_points[i][0]), scale*self.rel_points[i][1])
 		
 	def update(self, dt, screen_size):
-		
+
+		self.position[0] += self.velocity[0] * dt
+		self.position[1] += self.velocity[1] * dt
+
+		if self.position[0] < 0:
+			self.position[0] = 0
+			self.velocity[0] *= -0.5
+		elif self.position[0] > screen_size[0]:
+			self.position[0] = screen_size[0]
+			self.velocity[0] *= -0.5
+		if self.position[1] < 0:
+			self.position[1] = 0
+			self.velocity[1] *= -0.5
+		elif self.position[1] > screen_size[1]:
+			self.position[1] = screen_size[1]
+			self.velocity[1] *= -0.5
+
 		self.real_points = []
-		for point_angle,point_radius in self.rel_points:
+
+		for point_angle, point_radius in self.rel_points:
 			angle = radians(self.angle) + point_angle
 			xp = point_radius * sin(angle)
 			yp = point_radius * cos(angle)
@@ -28,5 +49,5 @@ class Spaceship:
 
 	def draw(self, surface):
 
-		color = (255,255,255)
-		pygame.draw.aalines(surface,color,True,self.real_points)
+		color = (255, 255, 255)
+		pygame.draw.aalines(surface, color, True, self.real_points)
